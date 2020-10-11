244459caeacb7c49cd99dc07c866944730fb7982
Vladislav Lemish <48563768+vlemish@users.noreply.github.com>
2020-10-11 23:59:24 +0300

    Update AppSettings.cs

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -7,12 +7,11 @@ namespace scheduleBot.Models
 {
     public abstract class AppSettings
     {
-        public static string Url { get; set; } = "https://telegrambotasp20200929195420.azurewebsites.net:443/{0}";//
+        public static string Url { get; set; } = "https://URL:443/{0}";
 
         public static string Name { get; set; } = "scheduleUniversityBot";
 
-        public static string Key { get; set; } = @"825359183:AAGcDxwSMoD5FMW_lWsQA-TcmeWY0wUgNZI";
-        //825359183:AAF3RWirEVg546WoqmMkaBlrapXYPYmM8Hk
-
+        public static string Key { get; set; } = @"API";
+        
     }
 }

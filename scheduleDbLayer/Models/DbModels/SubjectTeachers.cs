82e7dd7b494051e63465e71ecb6f4a291065fe17
vlemish <vlad.lemish123@gmail.com>
2020-11-01 17:44:59 +0200

    changes

---
+++ scheduleDbLayer/Models/DbModels/SubjectTeachers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scheduleDbLayer.Models
+{
+    public class SubjectTeachers
+    {
+        public SubjectTeachers(int teacherId, int subjectId)
+        {
+            TeacherId = teacherId;
+            SubjectId = subjectId;
+        }
+
+        public SubjectTeachers()
+        {
+
+        }
+
+
+        [Key, Column(Order = 1)]
+        public int SubjectId { get; set; }
+
+        [Key, Column(Order = 2)]
+        public int TeacherId { get; set; }
+
+        public Subject Subject { get; set; }
+
+        public Teacher Teacher { get; set; }
+
+    }
+}
